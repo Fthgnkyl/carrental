d90b770fd6c6a8874c2635192b16c1debbea934f
Fthgnkyl <68696004+Fthgnkyl@users.noreply.github.com>
2023-05-17 14:22:35 +0300

    Delete src directory

--- src/main/java/com/g4/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.g4.exception;
-
-public class ResourceNotFoundException extends RuntimeException{
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
